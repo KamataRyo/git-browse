3b9d2c99dd3c6345b631c1d5ea003ec0065fff61
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-09-22 13:12:58 +0900

    Add messageing

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # git-browse
+
 `git browse repo` opens your github repo with browser.
 
-## useage
+## Dependency
+
+Mac OS only
+
+## Usage
+
+Commands below open browser and locate the Github repositories.
+
 ```
 $ git browse user repo
 $ git browse user/repo
 $ git browse repo
+```
+
+## Install
+
+```
+$ npm i -g git-browse
 ```
--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 #!/usr/bin/env node
 var exec = require('child_process').exec
-var user, repo
+var username, reponame
 
 if (process.argv.length == 3) {
 	if (process.argv[2].includes('/')) {
-		[user, repo] = process.argv[2].split('/')
+		[username, reponame] = process.argv[2].split('/')
 	} else {
-		repo = process.argv[2]
+		reponame = process.argv[2]
 	}
 } else if (process.argv.length > 3) {
-	[user, repo] = [process.argv[2], process.argv[3]]
+	[username, reponame] = [process.argv[2], process.argv[3]]
 }
 
 (new Promise((fulfilled, rejected) =>{
-	if (!user) {
-		var getUser = 'echo $(git config --get user.name)'
-		exec(getUser, (err, stdout, stdin) => {
+	if (!username) {
+		var commandGetUser = 'echo $(git config --get user.name)'
+		exec(commandGetUser, (err, stdout, stdin) => {
 			if (err) {
 				rejected(err)
 			} else {
-				fulfilled(stdout.replace('\n', ''))
+				username = stdout.replace('\n', '')
+				console.log(`[info] locating to ${username}'s repository..`)
+				fulfilled(username)
 			}
 		})
 	} else {
-		fulfilled(user)
+		fulfilled(username)
 	}
-})).then((user) => {
+})).then((username) => {
 	// var host = process.env.npm_package_config_host
 	// var protocol = process.env.npm_package_config_protocol
 	// console.log(process.env)
-	// openHost = `open ${protocol}://${host}/${user}/${repo}`
-	openHost = `open https://github.com/${user}/${repo}`
-	console.log(openHost)
-	exec(openHost, (err, stdout, stdin) => {
-
+	// openHost = `open ${protocol}://${host}/${username}/${reponame}`
+	commandOpenHost = `open https://github.com/${username}/${reponame}`
+	console.log(`[info] Executing \`${commandOpenHost}\``)
+	exec(commandOpenHost, (err, stdout, stdin) => {
+		console.log('[info] done.')
 	})
+}).catch(() => {
+	console.log('[error] An error occured.')
+	console.log(error)
 })
